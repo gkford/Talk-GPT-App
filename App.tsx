4891d9768eda2c07cad2639f2e3b02795f5c7c36
gkford <gkford@gmail.com>
2024-01-12 15:11:49 +1300

    changes I didn't save before leaving for weeks - yikes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
+  const [borderColor, setBorderColor] = useState<"lightgray" | "lightgreen">("lightgray")
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 5}}>Outline Builder</Text>
+      <Text 
+        style={{
+          textAlign: "center",
+          color: "#333333",
+          fontSize: 12, 
+          marginBottom: 5
+        }}
+        >Press and hold to record your voice. Release to send the recording. Then you'll get a response to your message.</Text>
+      <Text style={{ marginVertical: 10, fontSize: 17}}>Your message: </Text>
+      <Pressable
+      onPressIn={() => {
+        setBorderColor("lightgreen")
+      }}
+      onPressOut={() => {
+        setBorderColor("lightgray")
+      }}
+          style={{
+            width: "90%",
+            padding: 30,
+            gap: 10,
+            borderWidth: 3, 
+            alignItems: "center",
+            borderRadius: 10,
+            borderColor: borderColor
+          }}
+      >
+        <Text>Hold to speak</Text>
+      </Pressable>
       <StatusBar style="auto" />
+
+
+      <Button title="Replay last message" onPress={()=>{}}></Button>
     </View>
   );
 }
@@ -16,5 +49,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
 });
